8311edb7743c2a4645db95f7c4b0ebd5d681637d
pivnati <natanmuleta911@gmail.com>
2025-10-06 20:31:35 -1200

    install the button from the shadcn ui

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Button } from '@/components/ui/button'
+const Header = () => {
+  return (
+    <div>
+       <Button> Button</Button>
+    </div>
+  )
+}
+
+export default Header
