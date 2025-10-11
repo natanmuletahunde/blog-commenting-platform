b905d54e5302ff56bd9a17722648884889cf82e4
pivnati <natanmuleta911@gmail.com>
2025-10-10 22:09:58 -1200

    make the create-post page

--- src/app/dashboard/create-post/page.jsx
+++ src/app/dashboard/create-post/page.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
-
+'use client'
+import {useUser} from '@clerk/nextjs'
 const CreatePostPage = () => {
+    const {isSignedIn,user, isLoaded} = useUser();
+    if(!isLoaded){
+        return null;
+    }
+    if(isSignedIn && user.publicMetadata.isAdmin){
+      return <div>
+        CreatePost
+      </div>
+    }
+    else{
+        return (
+            <h1>
+                Access Denied. You do not have permission to view this page.
+            </h1>
+        )
+    }
   return (
     <div>
      CreatePostPage
