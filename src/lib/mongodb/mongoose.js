626da95e3d18c99fe9eb82628c3476a9b6e7a79a
pivnati <natanmuleta911@gmail.com>
2025-10-10 12:14:33 -1200

    create the dashboard and post folder

--- src/lib/mongodb/mongoose.js
+++ src/lib/mongodb/mongoose.js
@@ -1,22 +1,23 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
-let initialized = false;
+let isConnected = false; // Track connection state
 
-export const connect = async ()=>{
-    mongoose.set('strictQuery', true);
-    if(initialized){
-        console.log("MongoDB is already initialized");
-        return;
-    }
-    try {
-        await mongoose.connect(process.env.MONGODB_URI , {
-            dbName:'nextjs13',
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log("MongoDB connected");
-        initialized = true;
-    } catch (error) {
-        console.log('Error connecting to MongoDB:', error);
-    }
-}
+export const connect = async () => {
+  mongoose.set('strictQuery', true);
+
+  if (isConnected) {
+    console.log('✅ MongoDB is already connected');
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: 'nextjs13',
+    });
+
+    isConnected = db.connections[0].readyState === 1;
+    console.log('🚀 MongoDB connected successfully');
+  } catch (error) {
+    console.error('❌ Error connecting to MongoDB:', error);
+  }
+};
